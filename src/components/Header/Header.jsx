b6f3180300307e19297b93e188c9d724c28632d6
Jose Solis <jose.solis@globant.com>
2022-10-30 21:16:07 -0300

    adding create account modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,67 +1,107 @@
-import React, { Fragment } from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import Login from '../../pages/Login/Login';
-import useAuth from '../../hooks/useAuth';
-import { NavLink } from 'react-router-dom';
+import React, { Fragment } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import Login from "../../components/Modals/Login/Login";
+import useAuth from "../../hooks/useAuth";
+import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import './Header.css';
+import "./Header.css";
+import CreateAccount from "../Modals/CreateAccount/CreateAccount";
 
 const NavScrollExample = () => {
   const { auth, setAuth } = useAuth();
-    const [modalShow, setModalShow] = React.useState(false);
-    const navigate = useNavigate();
-    const handleLogOut = () => {
-      setAuth(null);
-      navigate("/departamentos")
-    }
+  const [modalLogin, setModalLogin] = React.useState(false);
+  const [modalCreateAccount, setModalCreateAccount] = React.useState(false);
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    setAuth(null);
+    navigate("/departamentos");
+  };
   return (
-    <Navbar className='navbar__page' sticky="top" bg="light" expand="lg">
+    <Navbar className="navbar__page" sticky="top" bg="light" expand="lg">
       <Container fluid>
-        <Nav.Link className='brand__link' as={NavLink} to="/">TurismoReal</Nav.Link>
+        <Nav.Link className="brand__link" as={NavLink} to="/">
+          TurismoReal
+        </Nav.Link>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            { auth?.usuario?.rol === "Funcionario" ? <Nav.Link as={NavLink} to="reservas">Reservas</Nav.Link>  
-              : auth?.usuario?.rol === "Administrador" ? 
-                <Fragment>
-                  <Nav.Link as={NavLink} to="reservas">Reservas</Nav.Link>
-                  <Nav.Link as={NavLink} to="clients">Usuarios</Nav.Link>
-                  <Nav.Link as={NavLink} to="departamentos">Departamentos</Nav.Link>
-                  <Nav.Link as={NavLink} to="informes">Informes</Nav.Link>
-                </Fragment> 
-                : 
-                <Fragment>
-                  <Nav.Link as={NavLink} to="reservas">Reservas</Nav.Link>
-                  <Nav.Link as={NavLink} to="departamentos">Departamentos</Nav.Link>
-                </Fragment> 
-            }
+            {auth?.usuario?.rol === "Funcionario" ? (
+              <Nav.Link as={NavLink} to="reservas">
+                Reservas
+              </Nav.Link>
+            ) : auth?.usuario?.rol === "Administrador" ? (
+              <Fragment>
+                <Nav.Link as={NavLink} to="reservas">
+                  Reservas
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="clients">
+                  Usuarios
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="departamentos">
+                  Departamentos
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="informes">
+                  Informes
+                </Nav.Link>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <Nav.Link as={NavLink} to="reservas">
+                  Reservas
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="departamentos">
+                  Departamentos
+                </Nav.Link>
+              </Fragment>
+            )}
           </Nav>
         </Navbar.Collapse>
-        { auth?.usuario?._id ? 
+        {auth?.usuario?._id ? (
           <Fragment>
-            <Navbar.Text className='me-4'>
-              { auth ? `Bienvenido ${auth?.usuario?.nombre}` : null}
+            <Navbar.Text className="me-4">
+              {auth ? `Bienvenido ${auth?.usuario?.nombre}` : null}
             </Navbar.Text>
             <Button variant="primary" onClick={handleLogOut}>
               Cerrar Sesion
-            </Button> 
+            </Button>
           </Fragment>
-          :
-          <Button variant="primary" onClick={() => setModalShow(true)}>
-            Ingresar
-          </Button>  
-        }
-        <Login show={modalShow} setModalShow={setModalShow} onHide={() => setModalShow(false)}/>
+        ) : (
+          <Fragment>
+            <Button
+              className="me-2"
+              variant="primary"
+              onClick={() => setModalLogin(true)}
+            >
+              Ingresar
+            </Button>
+            <Button
+              variant="secondary"
+              onClick={() => setModalCreateAccount(true)}
+            >
+              Crear Cuenta
+            </Button>
+          </Fragment>
+        )}
+        <Login
+          show={modalLogin}
+          setModalShow={setModalLogin}
+          onHide={() => setModalLogin(false)}
+        />
+        <CreateAccount
+          show={modalCreateAccount}
+          setModalCreateAccount={setModalCreateAccount}
+          onHide={() => setModalCreateAccount(false)}
+        />
       </Container>
     </Navbar>
   );
-}
+};
 
 export default NavScrollExample;
---
+++ src/components/Modals/CreateAccount/CreateAccount.jsx
@@ -0,0 +1,179 @@
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import useAuth from "../../../hooks/useAuth";
+import axios from "../../../api/axios";
+import { message } from "antd";
+import { useNavigate } from "react-router-dom";
+import "./CreateAccount.css";
+import "antd/dist/antd.css";
+const LOGIN_URL = "/user/login";
+const GET_USERS = "/user/";
+
+function CreateAccount(props) {
+  const { setAuth } = useAuth();
+  const { setModalCreateAccount, show } = props;
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPwd] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [rut, setRut] = useState("");
+  const [telefono, setTelefono] = useState("");
+
+  const handleSetEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSetPw = (event) => {
+    setPwd(event.target.value);
+  };
+
+  const handleSetNombre = (event) => {
+    setNombre(event.target.value);
+  };
+
+  const handleSetRut = (event) => {
+    setRut(event.target.value);
+  };
+  const handleSetTelefono = (event) => {
+    setTelefono(event.target.value);
+  };
+
+  const success = () => {
+    message.success("Has ingresado correctamente");
+  };
+  const error = () => {
+    message.error("Ha ocurrido un error, verifica tu contraseña");
+  };
+
+  const handleClose = () => setModalCreateAccount(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // try {
+    //   const body = JSON.stringify({ rut, password });
+    //   const config = {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   };
+    //   const response = await axios.post(LOGIN_URL, body, config);
+    //   if (response?.data.ok === true) {
+    //     success();
+    //     const accessToken = response?.data?.token;
+    //     setRut("");
+    //     setPwd("");
+    //     setIsValid(true);
+    //     setModalShow(false);
+    //     const token = {
+    //       headers: {
+    //         "x-token": accessToken,
+    //       },
+    //     };
+    //     const response2 = await axios.get(GET_USERS, token);
+    //     const dataInfo = {
+    //       usuario: response2.data.usuario,
+    //       token: accessToken,
+    //     };
+    //     setAuth(dataInfo);
+    //     navigate("/reservas");
+    //   } else {
+    //     error();
+    //     setModalShow(false);
+    //   }
+    // } catch (err) {
+    //   if (!err?.response) {
+    //     setErrMsg("No Server Response");
+    //   } else if (err.response?.status === 400) {
+    //     setErrMsg("Missing Username or Password");
+    //   } else if (err.response?.status === 401) {
+    //     setErrMsg("Unauthorized");
+    //   } else {
+    //     setErrMsg("Login Failed");
+    //   }
+    // }
+  };
+
+  return (
+    <Modal
+      show={show}
+      onHide={handleClose}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Crear Cuenta
+        </Modal.Title>
+      </Modal.Header>
+      <Form className="login__form" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="email">E-mail</Form.Label>
+          <Form.Control
+            type="email"
+            id="email"
+            autoComplete="off"
+            onChange={handleSetEmail}
+            value={email}
+            required
+            placeholder="Ingresa tu Correo"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Ingresa tu contraseña"
+            id="password"
+            onChange={handleSetPw}
+            value={password}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="nombre">Nombre</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ingresa tu Nombre"
+            id="nombre"
+            onChange={handleSetNombre}
+            value={nombre}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="rut">Rut</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Ingresa tu Rut"
+            id="rut"
+            onChange={handleSetRut}
+            value={rut}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="telefono">Telefono</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Ingresa tu Telefono"
+            id="telefono"
+            onChange={handleSetTelefono}
+            value={telefono}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Crear Cuenta
+        </Button>
+      </Form>
+    </Modal>
+  );
+}
+export default CreateAccount;
