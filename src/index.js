b86ed87fbd98a70af372c86031a8ae3fbb9cdc26
Jose Solis <cotosolis.d@gmail.com>
2022-11-10 23:58:28 -0300

    adding edit inventory fixed v4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -15,4 +14,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(console.log);
-serviceWorker.register()
+
